b82552feed09cd6983bc087265eed4e44e75a5ac
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-01-16 17:59:29 -0300

    custom server for service worker

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const { createServer } = require('https');
+const { createServer } = require('http');
 const { parse } = require('url');
+const { join } = require('path');
 const next = require('next');
-const fs = require('fs');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const options = {
-  key: fs.readFileSync('./certificates/server.key'),
-  cert: fs.readFileSync('./certificates/server.cert'),
-};
-
 app.prepare().then(() => {
-  createServer(options, (req, res) => {
+  createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(3000, err => {
+    const { pathname } = parsedUrl;
+
+    if (pathname === '/service-worker.js') {
+      const filePath = join(__dirname, '.next', pathname);
+      app.serveStatic(req, res, filePath);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, err => {
     if (err) throw err;
-    console.log('> Ready on https://localhost:3000');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
