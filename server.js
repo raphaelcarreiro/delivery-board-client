eaec98ed0a2e1ac8c9587ecacb58c14a3b2a4c91
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-01-16 18:13:31 -0300

    fix server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.prepare().then(() => {
     const { pathname } = parsedUrl;
 
     if (pathname === '/service-worker.js') {
-      const filePath = join(__dirname, '.next', pathname);
+      const filePath = join(__dirname, 'next', pathname);
       app.serveStatic(req, res, filePath);
     } else {
       handle(req, res, parsedUrl);
