d148161976dc620e73160710a2aae10ba50f3f14
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-01-21 09:58:51 -0300

    server express for heroku ssl redirect

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
-const { createServer } = require('http');
+const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect');
+const next = require('next');
 const { parse } = require('url');
 const { join } = require('path');
-const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+
+const handle = app.getRequestHandler();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dir: '.', dev });
-const handle = app.getRequestHandler();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname } = parsedUrl;
+    server.use(sslRedirect());
 
-    // handle GET request to /service-worker.js
-    if (pathname === '/service-worker.js') {
-      const filePath = join(__dirname, '.next', pathname);
+    server.get('*', (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const { pathname } = parsedUrl;
 
-      app.serveStatic(req, res, filePath);
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+      // handle GET request to /service-worker.js
+      if (pathname === '/service-worker.js') {
+        const filePath = join(__dirname, '.next', pathname);
+
+        app.serveStatic(req, res, filePath);
+      } else {
+        handle(req, res, parsedUrl);
+      }
+    });
+
+    server.listen(port, error => {
+      if (error) throw error;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
   });
-});
