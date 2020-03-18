5c5a9d550652f413a76ba6561ce821b4665ccd6d
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-03-17 21:41:43 -0300

    fix sitemap generator

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -3,56 +3,72 @@ const fs = require('fs');
 const builder = require('xmlbuilder');
 
 function sitemapGenerator() {
+  const restaurantId = process.env.RESTAURANT_ID;
+
   const sitemap = builder
     .create('urlset')
     .dec()
     .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
-  sitemap
-    .ele('url')
-    .ele('loc')
-    .txt(`${process.env.PUBLIC_URL}`);
-
-  sitemap
-    .ele('url')
-    .ele('loc')
-    .txt(`${process.env.PUBLIC_URL}/menu`);
-
-  sitemap
-    .ele('url')
-    .ele('loc')
-    .txt(`${process.env.PUBLIC_URL}/checkout`);
-
-  sitemap
-    .ele('url')
-    .ele('loc')
-    .txt(`${process.env.PUBLIC_URL}/login`);
-
   axios
-    .get(`${process.env.BASEURL_API}categories`, {
+    .get(`${process.env.BASEURL_API}restaurants`, {
       headers: {
-        RestaurantId: process.env.RESTAURANT_ID,
+        RestaurantId: restaurantId,
       },
     })
     .then(response => {
-      const categories = response.data;
-      categories.forEach(category => {
+      const restaurant = response.data;
+      if (restaurant) {
         sitemap
           .ele('url')
           .ele('loc')
-          .txt(`${process.env.PUBLIC_URL}/menu/${category.url}`);
-      });
+          .txt(restaurant.url);
+
+        sitemap
+          .ele('url')
+          .ele('loc')
+          .txt(`${restaurant.url}/menu`);
+
+        sitemap
+          .ele('url')
+          .ele('loc')
+          .txt(`${restaurant.url}/checkout`);
+
+        sitemap
+          .ele('url')
+          .ele('loc')
+          .txt(`${restaurant.url}/login`);
+
+        axios
+          .get(`${process.env.BASEURL_API}categories`, {
+            headers: {
+              RestaurantId: restaurantId,
+            },
+          })
+          .then(response => {
+            const categories = response.data;
+            categories.forEach(category => {
+              sitemap
+                .ele('url')
+                .ele('loc')
+                .txt(`${restaurant.url}/menu/${category.url}`);
+
+              sitemap.end({ pretty: true });
+              fs.writeFile(`public/sitemap.xml`, sitemap, 'utf8', err => {
+                if (err) {
+                  console.log(err);
+                }
+              });
+            });
+          })
+          .catch(() => {
+            console.log('There was an error on create sitemap.xml');
+          });
+      }
     })
     .catch(() => {
       console.log('There was an error on create sitemap.xml');
     });
-
-  sitemap.end({ pretty: true });
-  fs.writeFile(`public/sitemap.xml`, sitemap, 'utf8', err => {
-    if (err) {
-      console.log(err);
-    }
-  });
 }
 
 module.exports = sitemapGenerator;
