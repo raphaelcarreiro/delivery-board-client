6ea4a8e950280c3a9f84f1f478a746f48bd39344
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-05-20 14:52:54 -0300

    changes in facebook pixel

--- pages/_document.js
+++ pages/_document.js
@@ -20,8 +20,7 @@ export default class MyDocument extends Document {
 
   setFacebookPixel() {
     return {
-      __html: `
-      !function(f,b,e,v,n,t,s)
+      __html: `!function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -30,23 +29,18 @@ export default class MyDocument extends Document {
       s.parentNode.insertBefore(t,s)}(window, document,'script',
       'https://connect.facebook.net/en_US/fbevents.js');
       fbq('init', '${this.props.pixelId}');
-      fbq('track', 'PageView');
-      `,
+      fbq('track', 'PageView');`,
     };
   }
 
   setFacebokScript() {
     return {
-      __html: `
-      <noscript>
-      <img
+      __html: `<img
         height="1"
         width="1"
         style={{ display: 'none' }}
         src='https://www.facebook.com/tr?id=${this.props.pixelId}&ev=PageView&noscript=1'
-      />
-      </noscript>
-      `,
+      />`,
     };
   }
 
@@ -62,8 +56,8 @@ export default class MyDocument extends Document {
           )}
           {pixelId && (
             <>
-              <script dangerouslySetInnerHTML={this.setFacebookPixel()} />
-              <noscript dangerouslySetInnerHTML={this.setFacebokScript()} />
+              <script dangerouslySetInnerHTML={this.setFacebookPixel()}></script>
+              <noscript dangerouslySetInnerHTML={this.setFacebokScript()}></noscript>
             </>
           )}
           <meta charSet="utf-8" />
