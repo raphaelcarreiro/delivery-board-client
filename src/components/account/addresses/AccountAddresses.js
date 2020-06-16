d7a3d13250599a6ca5ebcc3fa518e1d3f41cd41e
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-06-16 16:06:43 -0300

    fix account addresses layout

--- src/components/account/addresses/AccountAddresses.js
+++ src/components/account/addresses/AccountAddresses.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import AccountAddressesNew from './AccountAddressesNew';
 import AccountAddressesEdit from './AccountAddressesEdit';
 import { api } from '../../../services/api';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { MessagingContext } from '../../messaging/Messaging';
 import {
   addCustomerAddress,
