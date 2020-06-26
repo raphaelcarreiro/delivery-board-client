ccada1cc07bb0ec82e5838680d9fbd1b3eec6bf1
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2020-06-26 17:48:41 -0300

    added request on window focus in order

--- src/components/account/orders/order/Order.js
+++ src/components/account/orders/order/Order.js
@@ -89,6 +89,24 @@ export default function Order({ cryptId }) {
   const classes = useStyles();
 
   useEffect(() => {
+    window.addEventListener('focusin', () => {
+      api()
+        .get(`/orders/status/${cryptId}`)
+        .then(response => {
+          const orderStatus = response.data.reverse().map(status => {
+            const statusDate = parseISO(status.created_at);
+            status.formattedDate = format(statusDate, "PP 'às' p", { locale: ptbr });
+            return status;
+          });
+          setOrder(oldOrder => ({
+            ...oldOrder,
+            order_status: orderStatus,
+          }));
+        });
+    });
+  }, [cryptId]);
+
+  useEffect(() => {
     const socket = io.connect(process.env.URL_NODE_SERVER + '/client');
     if (order) {
       socket.emit('register', order.id);
