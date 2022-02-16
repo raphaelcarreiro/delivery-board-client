bfeca96fb2ea95659cfd3b417e6d4a71c24b2824
raphaelcarreiro <raphaelcarreiro@hotmail.com>
2022-02-16 08:01:13 -0300

    Ajusta texto info window no mapa

--- src/providers/google-maps/infoWindowContent.ts
+++ src/providers/google-maps/infoWindowContent.ts
@@ -1,6 +1,6 @@
 export const infoWindowContent = `
   <div style="padding: 12px; display: flex; flex-direction: column; text-align: center; font-size: 13px">
-   <span style="margin-bottom: 3px; font-weight: 400;">Você está aqui?</span>
+   <span style="margin-bottom: 3px; font-weight: 400;">Você está aqui perto?</span>
    <span style="color: #666">Ajuste sua localização</span>
   </div>
 `;
