cfc0dfb1f826905411bf66e1decb7c4290a06a10
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2026-01-11 15:13:20 -0300

    feat: add PropsWithChildren type to AuthLayout component

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useApp } from 'src/providers/AppProvider';
 
@@ -23,7 +23,7 @@ const useStyles = makeStyles({
   },
 });
 
-const AuthLayout: React.FC = ({ children }) => {
+const AuthLayout: React.FC<PropsWithChildren> = ({ children }) => {
   const { windowHeight } = useApp();
   const classes = useStyles({ windowHeight });
 
